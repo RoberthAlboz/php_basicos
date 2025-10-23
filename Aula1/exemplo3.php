941197a9b3371ae66303f1783c818783311f64a4
Roberth D. Alboz <roberth.alboz@aluno.senai.br>
2025-10-23 08:32:56 -0300

    desafio de ideias

---
+++ Aula1/exemplo3.php
@@ -0,0 +1,7 @@
+<?php 
+echo                   "Testando espaço em branco <br>"
+echo "Testando espaço <br>";
+
+// O caracter &nbsp adiciona espaço em branco dentro da string
+echo "&#160; &nbsp;           Testando espaço    em        branco";
+echo
